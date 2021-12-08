01c8361a76e1c04d79b47271f28d86c92815d1f8
szsupunma <86388269+szsupunma@users.noreply.github.com>
2021-12-07 17:49:33 -0800

    not yet

--- Yukki/Inline/play.py
+++ Yukki/Inline/play.py
@@ -8,23 +8,25 @@ def url_markup(videoid, duration, user_id, query, query_type):
     buttons = [
         [
             InlineKeyboardButton(
-                text="❮",
-                callback_data=f"slider B|{query_type}|{query}|{user_id}",
-            ),
-            InlineKeyboardButton(
-                text="Play Now",
+                text="▶️ Start Player",
                 callback_data=f"Yukki {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="❯",
-                callback_data=f"slider F|{query_type}|{query}|{user_id}",
+                text="🔎Search More",
+                callback_data=f"Search {query}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="🔎 More Results",
-                callback_data=f"Search {query}|{user_id}",
+                text="◀️Back",
+                callback_data=f"slider B|{query_type}|{query}|{user_id}",
             ),
+            InlineKeyboardButton(
+                text="Next▶️",
+                callback_data=f"slider F|{query_type}|{query}|{user_id}",
+            ),
+        ],
+         [
             InlineKeyboardButton(
                 text="🗑 Close Search",
                 callback_data=f"forceclose {query}|{user_id}",
