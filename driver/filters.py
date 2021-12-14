3a2868bbbdc1677dd957a95c4c3ad53748bc2eca
supun maduranga <83456156+supunmadurangasl@users.noreply.github.com>
2021-12-13 18:08:54 -0800

    hm

--- driver/filters.py
+++ driver/filters.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from typing import List, Union
-from config import ASSISTANT_PREFIX
+from config import COMMAND_PREFIXES
 
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
@@ -10,4 +10,4 @@ other_filters2 = (
 
 
 def command(commands: Union[str, List[str]]):
-    return filters.command(commands, ASSISTANT_PREFIX)
+    return filters.command(commands, COMMAND_PREFIXES)
